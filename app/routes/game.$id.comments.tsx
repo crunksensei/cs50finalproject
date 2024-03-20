c23bdd99cb064d495f3bae5751208d0e019350bc
crunksensei <63222538+crunksensei@users.noreply.github.com>
2024-03-19 19:47:44 -0500

    added comments completely finished working

--- app/routes/game.$id.comments.tsx
+++ app/routes/game.$id.comments.tsx
@@ -1,4 +1,10 @@
-import { Form, useLoaderData, useNavigation } from "@remix-run/react";
+import {
+  Form,
+  useLoaderData,
+  useNavigation,
+  useParams,
+  useFetcher,
+} from "@remix-run/react";
 import { LoaderFunction, ActionFunctionArgs, redirect } from "@remix-run/node";
 import { db } from "~/utils/db.server";
 import { getSession, addComment } from "../utils/session.server";
@@ -12,47 +18,44 @@ type Comments = {
 };
 
 export async function action({ request, params }: ActionFunctionArgs) {
-
-  const session = await getSession(
-    request.headers.get("Cookie")
-  );
+  const session = await getSession(request.headers.get("Cookie"));
   if (!session.has("userId")) {
     return redirect("/login");
   }
   const form = await request.formData();
-  const comment = form.get("comment");  
+  const comment = form.get("comment");
   const userId = session.get("userId");
-  console.log(comment, typeof comment)
-  console.log(userId, typeof userId)
-  console.log(params.id, typeof params.id)
 
-  if (!comment || typeof comment != 'string' || !userId || typeof userId != 'string' || !params.id || typeof params.id != 'string') {
+  if (
+    !comment ||
+    typeof comment != "string" ||
+    !userId ||
+    typeof userId != "string" ||
+    !params.id ||
+    typeof params.id != "string"
+  ) {
     throw new Response("Error posting comment", { status: 500 });
   }
-  try{
-    await addComment(comment as string, userId as string, params.id as string)
-  }catch(e){
-    console.log(e)
-    throw new Response("User not found / server error", { status: 500 })
+  try {
+    await addComment(comment as string, userId as string, params.id as string);
+  } catch (e) {
+    console.log(e);
+    throw new Response("User not found / server error", { status: 500 });
   }
   return null;
-  // return redirect(`/game/${params.id}/comments`);
 }
-
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const session = await getSession(request.headers.get("Cookie"));
   const url = new URL(request.url);
-  const offset = parseInt(url.searchParams.get("offset") || "0", 10);
-  
+  const limit = parseInt(url.searchParams.get("limit") || "30");
   const data: Comments[] = await db.comment.findMany({
     where: {
       gameid: params.id,
     },
-    take: 30, // Number of comments to take
-    skip: offset, // Skip the number of comments based on the offset
+    take: limit,
     orderBy: {
-      timestamp: 'desc',
+      timestamp: "desc",
     },
   });
 
@@ -60,8 +63,19 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 };
 
 export default function Comments() {
+  const params = useParams();
+  const fetcher = useFetcher();
+  const gameId = params.id;
   const data = useLoaderData<Comments[]>();
   const navigation = useNavigation();
+  const limit = 30;
+  const handleLoadMore = () => {
+    const newLimit = limit + 30;
+    if (fetcher.data == undefined || fetcher.data.length > newLimit) {
+      fetcher.load(`/game/${gameId}/comments?limit=${newLimit}`);
+    }
+    //possibly have it autoload when the user scrolls to the bottom of the page or update every so often to show more comments if load more has been clicked or not
+  };
 
   return (
     <div className="rounded-lg boarder p-3">
@@ -73,33 +87,58 @@ export default function Comments() {
             className="w-full border border-teal-500 rounded-lg p-2"
           ></textarea>
           <input type="hidden" name="id" value={data.id} />
-          {navigation.state === 'submitting' ? (
+          {navigation.state === "submitting" ? (
             <button
-            type="submit"
-            disabled
-            className="bg-teal-800 px-4 py-2 rounded-lg text-white"
-          >
-            Loading...
-          </button>
-            ): (
-              <button
-            type="submit"
-            className="bg-teal-500 px-4 py-2 rounded-lg text-white"
-          >
-            Submit
-          </button>
-            )}
+              type="submit"
+              disabled
+              className="bg-teal-800 px-4 py-2 rounded-lg text-white"
+            >
+              Loading...
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="bg-teal-500 px-4 py-2 rounded-lg text-white"
+            >
+              Submit
+            </button>
+          )}
         </Form>
         <div className="space-y-4">
-        {data.map((e, index) => (
-          <div key={index} className="border-l-4 border-teal-500 pl-4 py-2 bg-gray-50 rounded">
-            <div className="font-semibold">{e.username}</div>
-            <div className="text-gray-700">{e.text}</div>
-            <div className="text-xs text-gray-500">{new Date(e.timestamp).toLocaleString()}</div>
-          </div>
-        ))}
-      </div>
-        <div className="mt-5 flex flex-col gap-y-3"></div>
+          {fetcher.data == undefined
+            ? data.map((e, index) => (
+                <div
+                  key={index}
+                  className="border-l-4 border-teal-500 pl-4 py-2 bg-gray-50 rounded"
+                >
+                  <div className="font-semibold">{e.username}</div>
+                  <div className="text-gray-700">{e.text}</div>
+                  <div className="text-xs text-gray-500">
+                    {new Date(e.timestamp).toLocaleString()}
+                  </div>
+                </div>
+              ))
+            : fetcher.data.map((e, index) => (
+                <div
+                  key={index}
+                  className="border-l-4 border-teal-500 pl-4 py-2 bg-gray-50 rounded"
+                >
+                  <div className="font-semibold">{e.username}</div>
+                  <div className="text-gray-700">{e.text}</div>
+                  <div className="text-xs text-gray-500">
+                    {new Date(e.timestamp).toLocaleString()}
+                  </div>
+                </div>
+              ))}
+        </div>
+        {data.length > 29 ? (
+          <button
+          onClick={handleLoadMore}
+          className="mt-4 bg-teal-500 hover:bg-teal-600 px-4 py-2 rounded-lg text-white transition duration-150 ease-in-out"
+        >
+          Load More
+        </button>
+        ): null}        
       </div>
     </div>
   );
